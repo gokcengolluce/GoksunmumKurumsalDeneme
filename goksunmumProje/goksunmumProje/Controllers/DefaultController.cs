403d82125a3450b8fff9567ea85dde61890c60f5
Gökçen Göllüce <gollucegokcen@gmail.com>
2023-07-04 13:46:04 +0300

    back-end dosyaları yüklendi.

---
+++ goksunmumProje/goksunmumProje/Controllers/DefaultController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace goksunmumProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult Blogpost1()
+        {
+            return View();
+        }
+        public ActionResult Blogpost2()
+        {
+            return View();
+        }
+        public ActionResult Blogpost3()
+        {
+            return View();
+        }
+        public ActionResult Blogpost4()
+        {
+            return View();
+        }
+        public ActionResult Blogpost5()
+        {
+            return View();
+        }
+        public ActionResult Blogpost()
+        {
+            return View();
+        }
+
+    }
+}
